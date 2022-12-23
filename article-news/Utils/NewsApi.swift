aa13dd320000aa7210a9539cd5adda2dbe1e130b
Roby <robysetiawan2409@gmail.com>
2022-12-23 16:34:36 +0700

    feat: create news api utils to get constant url service

---
+++ article-news/Utils/NewsApi.swift
@@ -0,0 +1,19 @@
+//
+//  NewsApi.swift
+//  article-news
+//
+//  Created by Roby Setiawan on 23/12/22.
+//
+
+import Foundation
+class NewsApi {
+    /**
+     * version of news api
+     */
+    static var version: String = "v2"
+
+    /**
+     * current base URL of the service news api
+     */
+    static var BASE_URL: String = "https://newsapi.org/"
+}
