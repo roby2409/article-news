afecbd34926d045272bacc0fe9892ce2f616077e
Roby <robysetiawan2409@gmail.com>
2022-12-23 16:35:16 +0700

    feat: create model of articles

---
+++ article-news/Models/Articles.swift
@@ -0,0 +1,38 @@
+//
+//  Articles.swift
+//  article-news
+//
+//  Created by Roby Setiawan on 23/12/22.
+//
+
+import Foundation
+
+// MARK: - Article
+struct Articles: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [ArticleElement]
+}
+
+// MARK: - ArticleElement
+struct ArticleElement: Codable {
+    let source: Source
+    let author: String?
+    let title, articleDescription: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: Date
+    let content: String?
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String
+}
