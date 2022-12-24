ac72f1844b8f4fa0ae6e0f51abdd5f448ca516b7
Roby <robysetiawan2409@gmail.com>
2022-12-24 20:04:32 +0700

    create: model of categories

---
+++ article-news/Models/Categories.swift
@@ -0,0 +1,13 @@
+//
+//  Categories.swift
+//  article-news
+//
+//  Created by Roby Setiawan on 24/12/22.
+//
+
+import UIKit
+
+struct Categories {
+    let nameOfCategory: String
+    let photoOfCategory: UIImage
+}
