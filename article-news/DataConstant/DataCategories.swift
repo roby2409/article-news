0d8526392a09672aa17884e5edecdacb46008027
Roby <robysetiawan2409@gmail.com>
2022-12-24 20:04:19 +0700

    feat: create dummies for static data categories

---
+++ article-news/DataConstant/DataCategories.swift
@@ -0,0 +1,22 @@
+//
+//  DataCategory.swift
+//  article-news
+//
+//  Created by Roby Setiawan on 24/12/22.
+//
+
+import UIKit
+var categories: [Categories] = [
+    Categories(
+           nameOfCategory: "Business", photoOfCategory: UIImage(named: "business")!),
+    Categories(
+           nameOfCategory: "Entertainment", photoOfCategory: UIImage(named: "entertainment")!),
+    Categories(
+           nameOfCategory: "Health", photoOfCategory: UIImage(named: "health")!),
+    Categories(
+           nameOfCategory: "Science", photoOfCategory: UIImage(named: "science")!),
+    Categories(
+           nameOfCategory: "Sports", photoOfCategory: UIImage(named: "sports")!),
+    Categories(
+           nameOfCategory: "Technology", photoOfCategory: UIImage(named: "technology")!),
+]
