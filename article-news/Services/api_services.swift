278f125f1567bcbb0216d151822b341bd63e231d
Roby <robysetiawan2409@gmail.com>
2022-12-26 03:23:18 +0700

    feat: refactor api services to get sources

--- article-news/Services/api_services.swift
+++ article-news/Services/api_services.swift
@@ -17,18 +17,14 @@ class APIService :  NSObject {
     }()
     
     
-    private let defaultService = NewsApi.BASE_URL + NewsApi.version
+    public let defaultService = NewsApi.BASE_URL + NewsApi.version
     
-    func apiToGetNews(route: String, completionHandler : @escaping (_ response:Articles?, _ errorCode: Int)-> Void) {
+    func apiToGetNews(_ url: URL, completionHandler : @escaping (_ response:Articles?, _ errorCode: Int)-> Void) {
         guard let apiKey: String = AppInsightsArticleNewsWrapper().generateAklInside()else{
-            completionHandler(nil, 1004)
+            completionHandler(nil, 1002)
             return
         }
-        guard let urlRequest = URL(string: defaultService + "/" + route) else {
-            completionHandler(nil, 404)
-            return
-        }
-        var request = URLRequest(url: urlRequest)
+        var request = URLRequest(url: url)
         request.timeoutInterval = 12
         request.httpMethod = "GET"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type") // the request is JSON
@@ -44,6 +40,42 @@ class APIService :  NSObject {
                     return
                 }
                 guard let mappedResponse = try? JSONDecoder().decode(Articles.self, from: receivedData) else {
+                    completionHandler(nil, 1001)
+                    return
+                }
+                completionHandler(mappedResponse, httpResponse.statusCode)
+            }else{
+                let getStatusCode = (error as NSError?)?.code
+                completionHandler(nil, getStatusCode ?? 500)
+            }
+            
+        })
+        task.resume()
+    }
+    
+    
+    func apiToGetSources(_ url: URL, completionHandler : @escaping (_ response:Sources?, _ errorCode: Int)-> Void) {
+        guard let apiKey: String = AppInsightsArticleNewsWrapper().generateAklInside()else{
+            completionHandler(nil, 1002)
+            return
+        }
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 12
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type") // the request is JSON
+        request.setValue("application/json", forHTTPHeaderField: "Accept") // the response expected to
+        request.setValue(apiKey, forHTTPHeaderField: "X-Api-Key")
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            if error == nil, let receivedData = data, let httpResponse = response as? HTTPURLResponse {
+                print("http status code : \(httpResponse.statusCode)")
+                guard  (200 ..< 299) ~= httpResponse.statusCode else {
+                    completionHandler(nil, httpResponse.statusCode)
+                    return
+                }
+                guard let mappedResponse = try? JSONDecoder().decode(Sources.self, from: receivedData) else {
                     completionHandler(nil, 1001)
                     return
                 }
